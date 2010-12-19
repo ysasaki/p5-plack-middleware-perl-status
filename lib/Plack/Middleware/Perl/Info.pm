11d76621154affffcb23ca0329a53f8a5d513e95
yoshi <yoshi@colinux.(none)>
2010-12-19 14:24:54 +0900

    initial commit

---
+++ lib/Plack/Middleware/Perl/Info.pm
@@ -0,0 +1,32 @@
+package Plack::Middleware::Perl::Info;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::Perl::Info -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::Perl::Info;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Perl::Info is
+
+=head1 AUTHOR
+
+Yoshihiro Sasaki E<lt>aloelight {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut

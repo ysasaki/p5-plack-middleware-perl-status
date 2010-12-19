2ff0307b7c089db036f9f7763d7fa76faed91089
yoshi <yoshi@colinux.(none)>
2010-12-19 18:04:57 +0900

    とりあえず作成

--- lib/Plack/Middleware/Perl/Info.pm
+++ lib/Plack/Middleware/Perl/Info.pm
@@ -1,7 +1,68 @@
 package Plack::Middleware::Perl::Info;
 use strict;
 use warnings;
-our $VERSION = '0.01';
+use parent qw(Plack::Middleware);
+use Plack::Util::Accessor qw(board base_dir path);
+use Parallel::Scoreboard;
+use Data::MessagePack;
+
+our $VERSION = '0.002';
+
+sub prepare_app {
+    my $self = shift;
+
+    if ( $self->base_dir ) {
+        $self->board(
+            Parallel::Scoreboard->new( base_dir => $self->base_dir ) );
+    }
+    else {
+        die "Perl::Info require base_dir option";
+    }
+}
+
+sub call {
+    my ( $self, $env ) = @_;
+
+    my $res;
+    if ( $self->path && $env->{PATH_INFO} eq $self->path ) {
+        $res = $self->_handle_perl_info($env);
+    }
+    else {
+        $res = $self->app->($env);
+    }
+
+    $self->set_state;
+
+    return $res;
+}
+
+sub set_state {
+    my $self = shift;
+    my $dat = [ keys %INC ];
+    $self->board->update( Data::MessagePack->pack($dat) );
+}
+
+sub _handle_perl_info {
+    my ( $self, $env ) = @_;
+
+    my @body;
+    my $stats = $self->board->read_all();
+	my %modules;
+    for my $pid ( sort { $a <=> $b } keys %$stats ) {
+        my $dat = Data::MessagePack->unpack( $stats->{$pid} );
+		if ( $dat and ref $dat eq 'ARRAY' ) {
+			$modules{$_} = 1 for @$dat;
+		}
+    }
+	push @body, "Loaded Modules\n";
+	for my $module ( sort keys %modules ) {
+		$module =~ s/\.pm$//;
+		$module =~ s/\//::/g;
+		push @body, "    $module\n";
+	}
+
+    return [ 200, [ 'Cotnent-Type' => 'text/plain; charset=utf-8' ], \@body ];
+}
 
 1;
 __END__
@@ -12,7 +73,14 @@ Plack::Middleware::Perl::Info -
 
 =head1 SYNOPSIS
 
-  use Plack::Middleware::Perl::Info;
+  use Plack::Builder;
+
+  builder {
+      enable "Plack::Middleware::Perl::Info",
+          path => '/perl-info',
+          base_dir => '/tmp/my-server';
+      $app;
+  };
 
 =head1 DESCRIPTION
 
